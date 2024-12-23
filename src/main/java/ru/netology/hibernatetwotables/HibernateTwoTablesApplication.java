b5e5039716d5929d99e0b8d70ecf155dd3b9d794
najmovich <najmovich@protei.ru>
2024-12-24 01:05:59 +0300

    Initial commit

---
+++ src/main/java/ru/netology/hibernatetwotables/HibernateTwoTablesApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.hibernatetwotables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTwoTablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateTwoTablesApplication.class, args);
+    }
+
+}
